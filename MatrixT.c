a160dd399638b1a9c3d9d0c61c5b6736dd6a13be
Davenb42 <66222338+Davenb42@users.noreply.github.com>
2022-08-28 18:38:37 -0600

    Test of matrix multiplication with threads

--- MatrixT.c
+++ MatrixT.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h> 
+#include <unistd.h>
 
 int main() {
+
 	unsigned short i, j, n;
 
     printf("Matrix Thread: \n");
@@ -10,6 +13,7 @@ int main() {
     scanf("%hu", &n);
     int matrix1[n][n];
     int matrix2[n][n];
+    int result[n][n];
     for(i = 0; i < n;i++) {
     	for(j = 0; j < n;j++) {
             matrix1[i][j] = rand()%100;
@@ -18,5 +22,39 @@ int main() {
     }
     time_t rawtime;
     time ( &rawtime );
+    void* multiply(void *arg){
+        for(i = 0; i < n;i++) {
+    	    for(j = 0; j < n;j++) {
+                result[i][j] = matrix1[i][j]*matrix2[j][i];
+    	    }
+        }    
+        return arg;
+    }
+    pthread_t threads[n];
+    for (i = 0; i < n; i++){
+        pthread_create(&threads[i], NULL, multiply, NULL);
+        pthread_join(threads[i], NULL);
+    }
+    /*
+    for(i = 0; i < n;i++) {
+        for(j = 0; j < n;j++) {
+            printf("%d ", matrix1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i = 0; i < n;i++) {
+        for(j = 0; j < n;j++) {
+            printf("%d ", matrix2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(i = 0; i < n;i++) {
+        for(j = 0; j < n;j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }*/
 	return 0;
 }
